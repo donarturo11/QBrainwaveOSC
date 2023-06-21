12db5a8be94bb35630dbb0188f1cef459f65bea1
Artur Wrona <arturwrona91@gmail.com>
2023-06-22 01:08:32 +0200

    Add comments for ThinkGear codes

--- src/QThinkGear/QThinkGearDataHandler.h
+++ src/QThinkGear/QThinkGearDataHandler.h
@@ -4,6 +4,22 @@
 #include <QtCore>
 #include "TGData.h"
 
+/**
+ *  Data CODE definitions:
+ *  (ThinkGearStreamParser.h)
+ *
+ *  PARSER_CODE_BATTERY            0x01
+ *  PARSER_CODE_POOR_QUALITY       0x02
+ *  PARSER_CODE_ATTENTION          0x04
+ *  PARSER_CODE_MEDITATION         0x05
+ *  PARSER_CODE_8BITRAW_SIGNAL     0x06
+ *  PARSER_CODE_RAW_MARKER         0x07
+ *  PARSER_CODE_RAW_SIGNAL         0x80
+ *  PARSER_CODE_EEG_POWERS         0x81
+ *  PARSER_CODE_ASIC_EEG_POWER_INT 0x83
+ */
+
+
 void QThinkGearDataHandle( unsigned char extendedCodeLevel,
                             unsigned char code,
                             unsigned char numBytes,
