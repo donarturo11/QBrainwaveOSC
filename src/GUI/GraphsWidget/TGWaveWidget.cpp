19367fd30e7d70351dd7951efd04b9b512ef3df1
Artur Wrona <arturwrona91@gmail.com>
2023-06-26 19:41:43 +0200

    Implement raw wave graph

--- src/GUI/GraphsWidget/TGWaveWidget.cpp
+++ src/GUI/GraphsWidget/TGWaveWidget.cpp
@@ -1,11 +1,12 @@
 #include "TGWaveWidget.h"
-#include <QValueAxis>
+#include <QPointF>
+
+int TGWaveWidget::_maxValuesToSave = 8;
 
 TGWaveWidget::TGWaveWidget(QString label, QWidget *parent) 
 : TGWidget(label, parent)
 {
-    _xpos = 0.000;
-    _offset = 0;
+    _cur_pos = 0;
     _series = new QLineSeries;
     init();
 }
@@ -13,32 +14,61 @@ TGWaveWidget::TGWaveWidget(QString label, QWidget *parent)
 void TGWaveWidget::init()
 {
     _label->setGeometry(0, 30, 100, 20);
-    _chartview->setGeometry(120,0,400,150);
+    _chartview->setGeometry(120,0,400,200);
     _chart->addSeries(_series);
     _chart->createDefaultAxes();
-    _chart->axes()[0]->setMax(8.0);
+    _chart->axes()[0]->setMax(4.0);
     _chart->axes()[0]->setMin(0.0);
     _chart->axes()[0]->hide();
     _chart->axes()[1]->setMax(1.0);
     _chart->axes()[1]->setMin(-1.0);
     _chart->axes()[1]->hide();
-    
+
+    _maxvals_spin = new QSpinBox(this);
+    _maxvals_label = new QLabel("Samples\nper point", this);
+
+    _maxvals_label->setGeometry(0,60,80,40);
+    _maxvals_spin->setGeometry(0,100,50,25);
+    _maxvals_spin->setRange(2,16);
+    _maxvals_spin->setValue(_maxValuesToSave);
+    connect(_maxvals_spin, &QSpinBox::valueChanged, this, &TGWaveWidget::setAccuracy);
+
     _series->setColor(0x00FF00);
-    while (_xpos <= 8.0) {
-        _series->append(_xpos, 0);
-        _xpos += 0.02;
+
+    for (int i=0; i<(512*4); i++) {
+        insertValue(0);
     }
 }
 
 void TGWaveWidget::insertValue(int val)
 {
-    double y = val/MAXRAWVALUE;
-    double x = _xpos;
-    _series->replace(_offset, x, y);
-    _xpos += 0.02;
-    _offset++;
-    if (_xpos >= 8.0) {
-        _xpos = 0;
-        _offset = 0;
+    double y = val/2048.0000f;
+    double x = (double) _cur_pos / SAMPLERATE;
+    _saved_values.push_back(y);
+    while(_saved_values.size() > _maxValuesToSave)
+        _saved_values.pop_front();
+    if ((_cur_pos % _maxValuesToSave) == 0)
+        update();
+    _cur_pos++;
+}
+
+void TGWaveWidget::update()
+{
+    if (_cur_pos<_maxValuesToSave) return;
+    QList<QPointF> new_serie;
+    int idx=_cur_pos;
+    double sum = 0;
+    double average = 0;
+    for (auto v = _saved_values.begin(); v != _saved_values.end(); v++) {
+        sum += *v;
+        idx++;
     }
+    average = sum / _maxValuesToSave;
+    _series->append(QPointF(_cur_pos/SAMPLERATE, average));
+
+    double min = ((_cur_pos - _maxValuesToSave)/SAMPLERATE) - 4;
+    double max = min+4;
+
+    _chart->axes()[0]->setMin(min);
+    _chart->axes()[0]->setMax(max);
 }
--- src/GUI/GraphsWidget/TGWaveWidget.h
+++ src/GUI/GraphsWidget/TGWaveWidget.h
@@ -1,9 +1,13 @@
 #ifndef TGWAVE_WIDGET_H
 #define TGWAVE_WIDGET_H
 #include "TGWidget.h"
+#include <QList>
 #include <QLineSeries>
+#include <QSpinBox>
+#include <QLabel>
 
 #define MAXRAWVALUE 2048.0000
+#define SAMPLERATE 512.0000
 
 class TGWaveWidget : public TGWidget
 {
@@ -12,12 +16,19 @@ public:
     TGWaveWidget(QString label, QWidget *parent = nullptr);
     void insertValue(int val);
     void init();
-    double _xpos;
-    int _offset;
+public slots:
+    void setAccuracy(int i) {
+        _maxValuesToSave = i;
+    }
 private:
+    void update();
+protected:
     QLineSeries *_series;
-    
-    
+    QSpinBox *_maxvals_spin;
+    QLabel *_maxvals_label;
+    QList<double> _saved_values;
+    static int _maxValuesToSave;
+    int _cur_pos;
 };
 
 #endif // TGWAVE_WIDGET_H
