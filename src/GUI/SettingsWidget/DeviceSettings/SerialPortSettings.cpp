b36a8eb5c7f757350faa76bd6fb050a930b9d9b3
Artur Wrona <arturwrona91@gmail.com>
2025-02-21 10:00:34 +0100

    Use special baudlist for ThinkGear

--- src/GUI/SettingsWidget/DeviceSettings/SerialPortSettings.cpp
+++ src/GUI/SettingsWidget/DeviceSettings/SerialPortSettings.cpp
@@ -4,21 +4,20 @@
 #include "DeviceTypes.h"
 #include "DeviceSettingsWidget.h"
 
-const DeviceType serialTypes[] {
-    {"ThinkGearStreamParser", "ThinkGear Stream Parser"},
-    {"TwoByteRawParser", "2-byte raw wave parser (unsigned 12-bit)"}
-};
-const QVector<qint32> standardBaudrates( {
-    115200, 57600, 38400, 19200, 9600
-}
-);
-
+#ifndef STANDARD_BAUDS
+#define STANDARD_BAUDS { 115200, 57600, 38400, 19200, 9600 }
+#endif
+#ifndef THINKGEAR_BAUDS
+#define THINKGEAR_BAUDS { 57600, 9600 }
+#endif
 
 SerialPortSettings::SerialPortSettings(QWidget *parent) :
     DeviceSettings(parent)
 {
-    for (auto t : serialTypes) {
-        _types.append(t);
+    for (auto t : deviceTypes) {
+        if (t.porttype == "serial") {
+            _types.append(t);
+        }
     }
     _type = nullptr;
     refreshBauds();
@@ -48,9 +47,10 @@ DeviceSettings* SerialPortSettings::create(QWidget *w)
 
 void SerialPortSettings::refreshBauds()
 {
-    _bauds.clear();
-    for (auto b : standardBaudrates) {
-        _bauds.append(b);
+    if (_type) {
+        _bauds = (_type->name == "ThinkGearStreamParser")
+                                 ? QList<qint32>(THINKGEAR_BAUDS)
+                                 : QList<qint32>(STANDARD_BAUDS);
     }
     emit baudrateListChanged();
 }
