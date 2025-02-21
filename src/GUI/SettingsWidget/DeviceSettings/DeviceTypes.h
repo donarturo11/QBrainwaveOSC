4ae49bb405f1295718c6bee028d49370a1dab703
Artur Wrona <arturwrona91@gmail.com>
2025-02-21 10:06:25 +0100

    Improve signals

--- src/GUI/SettingsWidget/DeviceSettings/DeviceTypes.h
+++ src/GUI/SettingsWidget/DeviceSettings/DeviceTypes.h
@@ -4,9 +4,14 @@
 #include <QVector>
 struct DeviceType
 {
+    QString porttype;
     QString name;
     QString displayedName;
 };
 
+inline QVector<DeviceType> deviceTypes {
+    {"serial", "ThinkGearStreamParser", "ThinkGear Stream Parser"},
+    {"serial", "TwoByteRawParser", "2-byte raw wave parser (unsigned 12-bit)"}
+};
 
 #endif // DEVICETYPES_H
